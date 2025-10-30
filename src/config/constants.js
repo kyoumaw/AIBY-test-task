1141801b680d8c2585f78c728c67b49b7af7a738
Dzmitry K. <gawrfan@gmail.com>
2025-10-30 05:25:40 +0300

    feat: add config

---
+++ src/config/constants.js
@@ -0,0 +1,25 @@
+export const PRICING = {
+  yearlyPerYear: '$39.99',
+  yearlyPerWeek: '$0.48',
+  weeklyPrice: '$6.99',
+};
+
+export const SELECTORS = {
+  app: '#app',
+  pricePerYear: '.price-per-year',
+  priceYearlyPerWeek: '.price-per-week-yearly',
+  pricePerWeek: '.price-per-week',
+  continueButton: '.continue-button',
+  i18nElements: '[data-i18n]',
+};
+
+export const TRANSLATION_KEYS = {
+  justPricePerYear: 'Just {{price}} per year',
+  pricePerWeek: '{{price}} <br>per week',
+};
+
+export const LINKS = {
+  terms: 'https://apple.com/',
+  privacy: 'https://google.com/',
+  restore: 'https://google.com/',
+};
